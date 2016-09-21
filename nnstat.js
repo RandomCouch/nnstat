6887ed33b6ca479abf49d07f003d698e532d258a
RandomCouch <NicolasAbouHaidar@gmail.com>
2016-09-20 19:05:06 -0500

    update

--- nnstat.js
+++ nnstat.js
@@ -1,5 +1,6 @@
 var netstat = require('node-netstat');
 var ip = require('ip');
+var ps = require('ps-node');
 var trusted = require('./trusted.json');
 const dns = require('dns');
 
@@ -8,6 +9,7 @@ netstat({
 		protocol: 'tcp'
 	}
 }, function(item){
+	var pid = item.pid;
 	var protocol = item.protocol;
 	var localIP = item.local.address;
 	var localPort = item.local.port;
@@ -17,6 +19,7 @@ netstat({
 	var tr = false;
 	var hName = "";
 	var serviceName = "";
+	var processName = "";
 	
 	if(!ip.isPrivate(remoteIP) && remoteIP != null && remoteIP != ""){
 		//lookup IP
@@ -39,19 +42,46 @@ netstat({
 					}
 				}
 				
-				if(tr){
-					console.log(hName + " is trusted");
-				}else{
-					console.log(hName + " IS SUSPICIOUS");
-					console.log(remoteIP + ":" + remotePort);
-					//console.log(state);
-				}
+				ps.lookup({pid: pid}, function(err, resultList){
+					if(err){
+						console.log(err);
+					}else{
+						var process = resultList[0];
+						if(process){
+							
+							var cmd = process.command;
+							var cmdSplit = cmd.split("\\");
+							var file = cmdSplit[cmdSplit.length - 1];
+							if(file == ""){
+								file = "Not Found";
+							}
+							console.log("PROCESS: " + file);
+							processName = file;
+						}else{
+							console.log("Process not found");
+						}
+					
+					}
+						if(tr){
+							console.log(hName + " is trusted");
+						}else{
+							console.log(hName + " IS SUSPICIOUS");
+							console.log(remoteIP + ":" + remotePort);
+							//console.log(state);
+						}
+					
+					
+				});
+				
+				
+				
+				
 				
 				
 			}else{
 				//console.log("Error: " + err);
-				console.log(hName + " IS SUSPICIOUS");
-				console.log(remoteIP + ":" + remotePort);
+				//console.log(hName + " IS SUSPICIOUS");
+				//console.log(remoteIP + ":" + remotePort);
 			}
 		});
 	}
